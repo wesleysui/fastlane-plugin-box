159ca8d046cc149bd959bef803c934fdce65ad54
Wesley Sui <wesleysui@gmail.com>
2017-11-09 15:47:09 +0800

    update

--- lib/fastlane/plugin/box/actions/box_action.rb
+++ lib/fastlane/plugin/box/actions/box_action.rb
@@ -1,15 +1,32 @@
+require 'boxr'
+
 module Fastlane
   module Actions
     class BoxAction < Action
       def self.run(params)
-        UI.message("The box plugin is working!")
+        Actions.verify_gem!('boxr')
         ipa_file = params[:ipa]
-        client = Boxr::Client.new  #uses ENV['BOX_DEVELOPER_TOKEN']
-        # folder = client.folder_from_path('/some/directory/structure')
-        # file = client.upload_file(ipa_file, folder)
-        file = client.upload_file(ipa_file)
+        UI.message("filename: #{ipa_file}")
+
+        #JWT methods
+        token = Boxr::get_enterprise_token(private_key: ENV['JWT_PRIVATE_KEY'],
+                                    private_key_password: ENV['JWT_PRIVATE_KEY_PASSWORD'],
+                                    public_key_id: ENV['JWT_PUBLIC_KEY_ID'],
+                                    enterprise_id: ENV['BOX_ENTERPRISE_ID'],
+                                    client_id: ENV['BOX_CLIENT_ID'],
+                                    client_secret: ENV['BOX_CLIENT_SECRET'])
+
+        # client = Boxr::Client.new  #uses ENV['BOX_DEVELOPER_TOKEN']
+        client = Boxr::Client.new(token)
+
+        folder = client.folder_from_path('/')
+        client.create_folder()
+        file = client.upload_file(ipa_file, folder)
         updated_file = client.create_shared_link_for_file(file, access: :open)
         puts "Shared Link: #{updated_file.shared_link.url}"
+
+        # items = client.folder_items(Boxr::ROOT)
+        # items.each {|i| puts i.name}
       end
 
       def self.description
@@ -31,11 +48,11 @@ module Fastlane
 
       def self.available_options
         [
-          # FastlaneCore::ConfigItem.new(key: :your_option,
-          #                         env_name: "BOX_YOUR_OPTION",
-          #                      description: "A description of your option",
-          #                         optional: false,
-          #                             type: String)
+          FastlaneCore::ConfigItem.new(key: :ipa,
+                                  env_name: "BOX_IPA_FILE",
+                               description: "the provided ipa file name",
+                                  optional: true,
+                                      type: String)
         ]
       end
 
